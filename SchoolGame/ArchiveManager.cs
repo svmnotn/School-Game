ea9ec8261acff3aed62d01a914687ac9cb5286ec
Victor M. Suarez <svmnotn@gmail.com>
2015-08-06 16:00:05 -0400

    properly split the code into it's three components
    Game content and data :)

---
+++ SchoolGame/ArchiveManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolGame {
+  public static class ArchiveManager {
+
+    public static Archive LoadArchive(string dir) {
+      return null;
+    }
+
+    public static void SaveArchive(string dir, Archive arc) {}
+  }
+}
