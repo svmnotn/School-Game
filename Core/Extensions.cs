d6fcfc10a0b9c702d9722dfb313e37eab3fefab0
Victor M. Suarez <svmnotn@gmail.com>
2015-08-12 18:27:05 -0400

    implement a more localized Image.FromFile method

--- Core/Extensions.cs
+++ Core/Extensions.cs
@@ -1,6 +1,7 @@
 ﻿namespace SchoolGame {
   using System;
   using System.Collections.Generic;
+  using System.Drawing;
 
   public static class Extensions {
     public static void Shuffle<T>(this List<T> list) {
@@ -13,6 +14,10 @@
         list[k] = list[n];
         list[n] = value;
       }
+    }
+
+    public static Image LoadImage(string dataPath, string relativeLoc) {
+      return Image.FromFile(dataPath + '\\' + relativeLoc);
     }
   }
 }
