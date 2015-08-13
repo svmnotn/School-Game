a8e2ed227a3e54304dd75543691808b18235b1eb
Victor M. Suarez <svmnotn@gmail.com>
2015-08-13 18:51:47 -0400

    format ArchiveManger

--- Core/ArchiveManager.cs
+++ Core/ArchiveManager.cs
@@ -1,9 +1,9 @@
 ﻿namespace SchoolGame {
-  using Newtonsoft.Json;
   using System.Collections.Generic;
   using System.IO;
   using System.IO.Compression;
   using Data;
+  using Newtonsoft.Json;
 
   public static class ArchiveManager {
     static JsonSerializerSettings settings = new JsonSerializerSettings() { DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate, Formatting = Formatting.Indented, NullValueHandling = NullValueHandling.Ignore };
@@ -16,7 +16,7 @@
       for(int i = 0; i < dirs.Length; i++) {
         var t = new Topic();
         var name = dirs[i].Split('\\');
-        t.name = name[name.Length-1];
+        t.name = name[name.Length - 1];
         t.questions = new Dictionary<int, List<Question>>();
         foreach(var q in JsonConvert.DeserializeObject<List<Question>>(File.ReadAllText(dirs[i] + @"\questions.json"), settings)) {
           t.AddQuestion(q);
@@ -30,8 +30,8 @@
       File.WriteAllText(to + @"\info.json", JsonConvert.SerializeObject(arc, settings));
       File.WriteAllText(to + @"\settings.json", JsonConvert.SerializeObject(arc.settings, settings));
       foreach(var t in arc.topics) {
-        var n = to + @"\" + t.name;
-        Directory.CreateDirectory(to + @"\" + t.name);
+        var n = to + '\\' + t.name;
+        Directory.CreateDirectory(to + '\\' + t.name);
         var content = new List<Question>();
         foreach(var qList in t.questions.Values) {
           foreach(var q in qList) {
