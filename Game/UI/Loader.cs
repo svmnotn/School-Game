5bd04bcf390ae6a04e9c49c2688dd50566302176
Victor M. Suarez <svmnotn@gmail.com>
2015-08-16 11:29:02 -0400

    finish loader functionality
    Hopefully...

--- Game/UI/Loader.cs
+++ Game/UI/Loader.cs
@@ -9,10 +9,42 @@
 
     internal Loader() {
       InitializeComponent();
+      LoadArchivesToTable();
+    }
+
+    internal void LoadArchivesToTable() {
+      if(Directory.Exists(Program.ArchivesDirectory)) {
+        var dirs = Directory.GetDirectories(Program.ArchivesDirectory);
+        archivesTable.Controls.Clear();
+        archivesTable.RowStyles.Clear();
+        foreach(var dir in dirs) {
+          var archive = ArchiveManager.LoadArchiveFromDir(dir);
+          var control = new ArchiveControl(archive);
+          archivesTable.Controls.Add(control);
+        }
+      } else {
+        Directory.CreateDirectory(Program.ArchivesDirectory);
+      }
     }
 
     private void ImportArchive(object sender, EventArgs e) {
+      var res = openArchive.ShowDialog();
+      if(res == DialogResult.OK || res == DialogResult.Yes) {
+        var a = ArchiveManager.LoadArchiveFromFile(openArchive.OpenFile(), Program.TmpDirectory);
+        if(!Directory.Exists(Program.ArchivePath(a))) {
+          Directory.Move(Program.TmpDirectory, Program.ArchivePath(a));
+          LoadArchivesToTable();
+        } else {
+          //SHOW ERROR
+          Directory.Delete(Program.TmpDirectory, true);
+        }
+      }
+    }
 
+    internal void StartGame(Archive archive) {
+      Hide();
+      Program.main = new MainWindow(archive);
+      Program.main.Show();
     }
   }
 }
