a9e1166e221cfd76b6bc8e55edc9422c338afe44
Victor M. Suarez <svmnotn@gmail.com>
2015-08-16 11:17:31 -0400

    centralize the program directory

--- Core/Extensions.cs
+++ Core/Extensions.cs
@@ -2,8 +2,17 @@
   using System;
   using System.Collections.Generic;
   using System.Drawing;
+  using System.IO;
+  using System.IO.Compression;
+  using static System.Environment;
 
   public static class Extensions {
+    public static string CoreDirectory {
+      get {
+        return GetFolderPath(SpecialFolder.ApplicationData) + @"\School Game";
+      }
+    }
+
     public static void Shuffle<T>(this List<T> list) {
       Random rng = new Random();
       int n = list.Count;
